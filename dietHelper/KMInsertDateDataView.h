6ef8cfa400cedc5b05f66e25acb280adbb797542
koujimiura <kouji.miura@alfredcore.com>
2012-12-17 21:59:56 +0900

    新規データ追加モーダルビュー追加（バグ数件あり）

---
+++ dietHelper/KMInsertDateDataView.h
@@ -0,0 +1,13 @@
+//
+//  KMInsertDateDataView.h
+//  dietHelper
+//
+//  Created by KoujiMiura on 2012/12/17.
+//  Copyright (c) 2012年 KoujiMiura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMInsertDateDataView : UIView
+
+@end
---
+++ dietHelper/KMInsertDateDataView.m
@@ -0,0 +1,31 @@
+//
+//  KMInsertDateDataView.m
+//  dietHelper
+//
+//  Created by KoujiMiura on 2012/12/17.
+//  Copyright (c) 2012年 KoujiMiura. All rights reserved.
+//
+
+#import "KMInsertDateDataView.h"
+
+@implementation KMInsertDateDataView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
